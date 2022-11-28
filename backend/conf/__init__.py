ffed2f13d22d83e2b5b36030076345f1177c448d
bfl-aug <songjunliang@8lab.cn>
2022-11-28 11:26:03 +0800

    fix: conf路径修改

--- backend/conf/__init__.py
+++ backend/conf/__init__.py
@@ -1,5 +1,6 @@
+from pathlib import Path
 from t.config import json2object
 
-CONF_JSON = "conf/conf.json"
+CONF_JSON = str(Path(__file__).resolve().parent.parent) + "/conf/conf.json"
 
 config = json2object(CONF_JSON)
