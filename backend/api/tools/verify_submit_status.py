64339390de62d1ed994fb3a4c839851c0805a26c
bfl-aug <songjunliang@8lab.cn>
2023-02-06 14:33:26 +0800

    fix: Modify the detection content and increase the number of detection submissions

--- backend/api/tools/verify_submit_status.py
+++ backend/api/tools/verify_submit_status.py
@@ -5,19 +5,33 @@
 @Time        : 2023/1/16 12:27
 @Description :
 """
+import datetime
+import time
+
 from conf import config
 from api.tools import detection_result
 
+# 每日检测次数
 number_of_detection = config.number_of_detection
 
 
 def checkstatus(doc, result):
+    """
+    :return :
+        0没问题1正在检测2检测超次数
+    """
     if check_whether_there_is_detect(doc, result):
         first = doc.first()
         return False, {"code": 200, "status": 1, "msg": "one is currently being detected", "id": first.id}
 
-    if check_detection_numberOfTimes(doc.count()):
-        return False, {"code": 200, "status": 2, "msg": f"{number_of_detection} have been detected"}
+    zero = int(time.mktime(time.strptime(str(datetime.date.today()), '%Y-%m-%d')))
+    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+    last = int(time.mktime(time.strptime(str(tomorrow), '%Y-%m-%d'))) - 1
+    count = doc.filter(date__gte=zero, date__lte=last).count()
+    print("几次", count)
+    if check_detection_numberOfTimes(count):
+        return False, {"code": 200, "status": 2,
+                       "msg": f"This version is limited to three times a day, please try again tomorrow"}
     return True, ""
 
 
@@ -25,18 +39,15 @@ def check_whether_there_is_detect(doc, result):
     """检查是否有正在检测中的
     :param doc:
     """
-    corethril = result.get("corethril")
     core_slither = result.get("core_slither")
 
     if doc.filter(result={}).exists():
         return True
+    if not core_slither and core_slither != []:
+        return True
     status, _ = detection_result.parseErrorResult(doc.first().id)
     if status:
         return False
-    if not corethril and corethril != []:
-        return True
-    if not core_slither and core_slither != []:
-        return True
     return False
 
 
